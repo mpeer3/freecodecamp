c40986c09db80da81e9afc6aceb9431465d22034
Michael Peer <mpeer3@yahoo.de>
2019-08-02 18:32:37 +0200

    Solution update

--- challenges/check_for_palindromes.js
+++ challenges/check_for_palindromes.js
@@ -1,20 +1,13 @@
-/* Solution by Michael Peer 2015*/
-/* http://www.freecodecamp.com/challenges/bonfire-check-for-palindromes */
-/* Convert to lowercase and remove any special characters; reverse and compare
-   with itself */
+/* Solution by Michael Peer 2019*/
 
 function palindrome(str) {
-  str=str.toLowerCase();
-  str=str.replace(/ /g,"");
-  str=str.replace(/\,/g,"");
-  str=str.replace(/\./g,"");
-  str=str.replace(/\(/g,"");
-  str=str.replace(/\)/g,"");
-  str=str.replace(/\//g,"");
-  str=str.replace(/\\/g,"");
-  str=str.replace(/\_/g,"");
-  str=str.replace(/\-/g,"");
-  var str2=str.split("").reverse().join("");
-  return str===str2;
+  // Good luck!
+  str=str.replace(/\W/g,'').toLowerCase()
+  str=str.replace(/\_/g,'').toLowerCase()
+  var str2=str.split('').reverse().join('')
+  return str==str2;
 }
+
+
+
 palindrome("eye");
