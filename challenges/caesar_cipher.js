2c3b7a594e827f5582d2232495f2b6207721b519
Michael Peer <mpeer3@yahoo.de>
2019-08-02 18:48:38 +0200

    Solution update

--- challenges/caesar_cipher.js
+++ challenges/caesar_cipher.js
@@ -1,26 +1,25 @@
-function rot13(str) { // LBH QVQ VG!
-  var res="";
-  var alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //26
-  var arr_alpha=alpha.split("");
-  var arr_input=str.toUpperCase().split("");
-  for (var ct=0;ct<arr_input.length;ct++){
-     var pos=alpha.indexOf(arr_input[ct]);
-     if (pos<0){
-       res=res+arr_input[ct];
-     } else{
-       pos=pos+13;
-       if (pos >=26){
-         pos=pos-26;
-       }
-       res=res+alpha[pos];
-     }
-  }
-  console.log(arr_input);
+//By Michael Peer 2019
 
-  console.log(arr_alpha.length);
-  return res;
+const alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ" //26
+const shiftLetter=(letter)=>{
+  let pos=alpha.indexOf(letter);
+  if (alpha.indexOf(letter)>=0){
+    pos=pos+13;
+    if (pos >=26){
+      pos=pos-26;
+    }
+    return alpha[pos];
+   } else {
+     return letter;
+   }
 }
 
 
-rot13("SERR PBQR PNZC");
+const rot13=(str) =>{ // LBH QVQ VG!
+  
+ 
+  return str.split("").map(shiftLetter).join("");
+}
 
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
